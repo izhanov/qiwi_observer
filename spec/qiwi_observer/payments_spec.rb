35f2485addb2fdbdcd0626bb29f0186732995a81
Izhanov <ibekable@gmail.com>
2019-06-10 13:49:34 +0600

    Fixed test's: Added webmock stub's

--- spec/qiwi_observer/payments_spec.rb
+++ spec/qiwi_observer/payments_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe QiwiObserver::Payments do
   describe "#call" do
     context "when arguments don't set" do
       it "returns with 'Error 422 Unprocessable Entity' message " do
+        stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77002571217/payments").
+          with(
+            headers: {
+           'Accept'=>'application/json',
+           'Authorization'=>'Bearer 35bf0ae0b5a2fdd8e29b1806ae7456d1',
+           'Content-Type'=>'application/json'
+            }).
+          to_return(status: [422, 'Unprocessable Entity'])
         payments = described_class.new(
           wallet: '77002571217',
           token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
@@ -28,15 +36,31 @@ RSpec.describe QiwiObserver::Payments do
 
     context "when arguments set" do
       it "returns hash with :data, :nextTxnId & :nextTxnDate keys" do
+        stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77774889669/payments?rows=10").
+          with(
+            headers: {
+           'Accept'=>'application/json',
+           'Authorization'=>'Bearer 3ef97bca7bf63c78a94898326485a6c8',
+           'Content-Type'=>'application/json'
+            }).
+          to_return(status: 200, body: File.new('spec/qiwi_observer/fixtures/payments.json'))
         payments = described_class.new(
-          wallet: '77002571217',
-          token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
+          wallet: '77774889669',
+          token: '3ef97bca7bf63c78a94898326485a6c8'
         ).call({rows: 10})
         expect(payments.value).to include(:data, :nextTxnId, :nextTxnDate)
       end
 
       context "when token is not correct" do
         it "returns with 'Error 401 Unauthorized' message " do
+          stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77002571217/payments?rows=10").
+            with(
+              headers: {
+             'Accept'=>'application/json',
+             'Authorization'=>'Bearer 35bf0ae0b5a2fdd8e29b1806ae7456d2',
+             'Content-Type'=>'application/json'
+              }).
+            to_return(status: [401, 'Unauthorized'])
           payments = described_class.new(
             wallet: '77002571217',
             token: '35bf0ae0b5a2fdd8e29b1806ae7456d2'
@@ -47,6 +71,14 @@ RSpec.describe QiwiObserver::Payments do
 
       context "when wallet is not correct" do
         it "returns with 'Error 403 Forbidden' message " do
+          stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77002571219/payments?rows=10").
+            with(
+              headers: {
+             'Accept'=>'application/json',
+             'Authorization'=>'Bearer 35bf0ae0b5a2fdd8e29b1806ae7456d1',
+             'Content-Type'=>'application/json'
+              }).
+            to_return(status: [403, 'Forbidden'])
           payments = described_class.new(
             wallet: '77002571219',
             token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
