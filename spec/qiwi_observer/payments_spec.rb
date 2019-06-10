d4b674467e2c31f0eb9c69f6ed1ac2f031b9452b
Izhanov <ibekable@gmail.com>
2019-06-10 10:02:46 +0600

    Refactoring tests names

--- spec/qiwi_observer/payments_spec.rb
+++ spec/qiwi_observer/payments_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe QiwiObserver::Payments do
   end
 
   describe "#call" do
-    context "when args don't set" do
-      it "response returns with '422 Unprocessable Entity' message " do
+    context "when arguments don't set" do
+      it "returns with 'Error 422 Unprocessable Entity' message " do
         payments = described_class.new(
           wallet: '77002571217',
           token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
@@ -26,13 +26,33 @@ RSpec.describe QiwiObserver::Payments do
       end
     end
 
-    context "when correct arguments set" do
-      it "response returns hash with :data, :nextTxnId & :nextTxnDate keys" do
+    context "when arguments set" do
+      it "returns hash with :data, :nextTxnId & :nextTxnDate keys" do
         payments = described_class.new(
           wallet: '77002571217',
           token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
         ).call({rows: 10})
         expect(payments.value).to include(:data, :nextTxnId, :nextTxnDate)
+      end
+
+      context "when token is not correct" do
+        it "returns with 'Error 401 Unauthorized' message " do
+          payments = described_class.new(
+            wallet: '77002571217',
+            token: '35bf0ae0b5a2fdd8e29b1806ae7456d2'
+          ).call({rows: 10})
+          expect(payments.error).to eq('Error 401 Unauthorized')
+        end
+      end
+
+      context "when wallet is not correct" do
+        it "returns with 'Error 403 Forbidden' message " do
+          payments = described_class.new(
+            wallet: '77002571219',
+            token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
+          ).call({rows: 10})
+          expect(payments.error).to eq('Error 403 Forbidden')
+        end
       end
     end
   end
