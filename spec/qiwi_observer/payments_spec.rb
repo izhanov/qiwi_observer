48d43cc31597f79f592e782eb4333eb9da0a6f51
Izhanov <ibekable@gmail.com>
2019-06-12 08:53:09 +0600

    Refactoring code

--- spec/qiwi_observer/payments_spec.rb
+++ spec/qiwi_observer/payments_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe QiwiObserver::Payments do
 
   it "is create with wallet and token" do
     payments = described_class.new(
-      wallet: '77002571217',
-      token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
+      wallet: '77002571218',
+      token: '35bf0ae0b5a2fdd8e29b1806ae7456d2'
     )
     expect(payments).to be_instance_of(described_class)
   end
@@ -18,17 +18,17 @@ RSpec.describe QiwiObserver::Payments do
   describe "#call" do
     context "when arguments don't set" do
       it "returns with 'Error 422 Unprocessable Entity' message " do
-        stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77002571217/payments").
+        stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77002571218/payments").
           with(
             headers: {
            'Accept'=>'application/json',
-           'Authorization'=>'Bearer 35bf0ae0b5a2fdd8e29b1806ae7456d1',
+           'Authorization'=>'Bearer 35bf0ae0b5a2fdd8e29b1806ae7456d2',
            'Content-Type'=>'application/json'
             }).
           to_return(status: [422, 'Unprocessable Entity'])
         payments = described_class.new(
-          wallet: '77002571217',
-          token: '35bf0ae0b5a2fdd8e29b1806ae7456d1'
+          wallet: '77002571218',
+          token: '35bf0ae0b5a2fdd8e29b1806ae7456d2'
         ).call
         expect(payments.error).to eq('Error 422 Unprocessable Entity')
       end
@@ -88,5 +88,4 @@ RSpec.describe QiwiObserver::Payments do
       end
     end
   end
-
 end
