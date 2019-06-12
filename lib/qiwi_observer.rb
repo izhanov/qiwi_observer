490abcc2aca8c81ee563e48cfe53656d7eb93d0b
Izhanov <ibekable@gmail.com>
2019-06-12 11:17:44 +0600

    Files & modules are required: 1) openssl; 2) base64; 3) json; 4) webhook.rb 5) webhook_result.rb 6) response.rb

--- lib/qiwi_observer.rb
+++ lib/qiwi_observer.rb
@@ -1,9 +1,14 @@
 require 'net/http'
 require "dry-configurable"
+require 'base64'
+require 'openssl'
+require 'json'
 
 require "qiwi_observer/version"
 require 'qiwi_observer/payments'
+require 'qiwi_observer/response'
 require 'qiwi_observer/webhook'
+require 'qiwi_observer/webhook_result'
 
 module QiwiObserver
   extend Dry::Configurable
