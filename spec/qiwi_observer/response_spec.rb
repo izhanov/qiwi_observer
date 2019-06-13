4e1be3d6ec99146e8a4b36bbcc61bcd61bd471ef
Izhanov <ibekable@gmail.com>
2019-06-13 16:16:28 +0600

    Removed #short_info test

--- spec/qiwi_observer/response_spec.rb
+++ spec/qiwi_observer/response_spec.rb
@@ -1,23 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe QiwiObserver::Response do
-  describe '#short_info' do
-    it 'returns array of hashes with :account_id, :amount, :date & :comment keys' do
-      stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77774889669/payments?operation=IN&rows=10").
-        with(
-          headers: {
-         'Accept'=>'application/json',
-         'Authorization'=>'Bearer 3ef97bca7bf63c78a94898326485a6c8',
-         'Content-Type'=>'application/json'
-          }).
-        to_return(status: 200, body: File.new('spec/qiwi_observer/fixtures/payments.json'))
-
-      request = QiwiObserver::Payments.new(wallet:'77774889669' , token: '3ef97bca7bf63c78a94898326485a6c8')
-      response = request.call({rows: 10, operation: 'IN'})
-      expect(response.short_info.first).to include(:account_id, :amount, :date, :comment)
-    end
-  end
-
   describe '#success?' do
     it 'returns true if response success' do
       stub_request(:get, "https://edge.qiwi.com/payment-history/v2/persons/77774889669/payments?operation=IN&rows=10").
